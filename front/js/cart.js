a12506fd6b831de374e86d6ab4e0b6af0c156e44
PaulMouchel <paulmouchel@live.fr>
2022-01-11 11:30:21 +0400

    modify quantity and delete products on cart page

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,14 +6,11 @@ function getCartProducts () {
         }
     })
     .then(function(productsList) {  
-        console.log(productsList)
         let cart = [];
         const localStorageCart = localStorage.getItem('cart')
         if(localStorageCart){
             cart = JSON.parse(localStorageCart);
         }
-
-        console.log(cart)
 
         cart = cart.map(product => {
             const index = productsList.findIndex(item => item._id === product.productId)
@@ -22,7 +19,8 @@ function getCartProducts () {
                 ...product,
                 name: data.name,
                 imageUrl: data.imageUrl,
-                price: data.price
+                price: data.price,
+                totalPrice: data.price * product.quantity
             }
         })
 
@@ -35,7 +33,7 @@ function getCartProducts () {
                     <div class="cart__item__content__description">
                         <h2>${product.name}</h2>
                         <p>${product.color}</p>
-                        <p>${product.price * product.quantity} €</p>
+                        <p>${product.totalPrice} €</p>
                     </div>
                     <div class="cart__item__content__settings">
                         <div class="cart__item__content__settings__quantity">
@@ -51,6 +49,19 @@ function getCartProducts () {
         `).join('')
 
         document.getElementById("cart__items").innerHTML = htmlCart
+
+        const quantityList = document.getElementsByClassName("itemQuantity");
+        for (let item of quantityList) {
+            item.addEventListener("change", changeQuantity)
+        }
+
+        const deleteList = document.getElementsByClassName("deleteItem");
+        for (let item of deleteList) {
+            item.addEventListener("click", removeFromCart)
+        }
+
+        document.getElementById("totalQuantity").innerText = cart.reduce((acc , val) => acc + val.quantity, 0)
+        document.getElementById("totalPrice").innerText = cart.reduce((acc , val) => acc + val.totalPrice, 0)
     })
     .catch(function(err) {
         console.log(err)
@@ -59,3 +70,38 @@ function getCartProducts () {
 }
 
 getCartProducts()
+
+function changeQuantity(e) {
+    const [ cart, index ] = getCartAndProductIndex(e)
+    cart[index].quantity = parseInt(e.target.value)
+    localStorage.setItem('cart', JSON.stringify(cart))
+    getCartProducts()
+}
+
+function removeFromCart(e) {
+    const [ cart, index ] = getCartAndProductIndex(e)
+    cart.splice(index, 1)
+    localStorage.setItem('cart', JSON.stringify(cart))
+    getCartProducts()
+}
+
+function getProductIdAndColor(e) {
+    return e.target.closest(".cart__item").dataset
+}
+
+function getCartIndex(cart, productId, color) {
+    return cart.findIndex(cartItem => cartItem.color === color && cartItem.productId === productId)
+}
+
+function getCartAndProductIndex(e) {
+    const { id, color } = getProductIdAndColor(e)
+    
+    const localStorageCart = localStorage.getItem('cart')
+    if(localStorageCart){
+        cart = JSON.parse(localStorageCart);
+    }
+
+    const index = getCartIndex(cart, id, color)
+
+    return [cart, index]
+}
